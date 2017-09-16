c2fbf40df4f63572e1e54b26fbea390fdf23b354
Gerald Porter <porter.glp2010@gmail.com>
2017-09-15 22:27:34 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,15 +9,28 @@ var totalscoremsg = document.getElementById("total-score-msg");
 var totalscore = document.getElementById("total-score");
 
 
+// JavaScript function that wraps everything
+
+
 setRandomNumber();
 //initialize wincount msg
+wincountlabel.textContent = "Wins:  "
 wincount.textContent = "0";
+losscountlabel.textContent = "Losses: "
 losscount.textContent = "0";
 //gamePick.innerHTM;
 totalscoremsg.textContent = "You total score is:"
 totalscore.textContent = 0;
 
+loadCrystals();
 
+//initialize crystal value
+function loadCrystals() {
+    document.getElementById("crystal-1").alt = pickRandomNumForCrystal().toString();
+    document.getElementById("crystal-2").alt = pickRandomNumForCrystal().toString();
+    document.getElementById("crystal-3").alt = pickRandomNumForCrystal().toString();
+    document.getElementById("crystal-4").alt = pickRandomNumForCrystal().toString();
+} //end setCrystalRandomNum function
 
 function setRandomNumber() {
     //set user nuber to guess 
@@ -32,49 +45,66 @@ function pickRandomNumForCrystal() {
 } //end pickRandomNumber function
 
 $("#crystal-1").on("click", function() {
-    crystalClicked();
+    var randNum = parseInt(document.getElementById("crystal-1").alt);
+    crystalClicked(randNum);
     //alert("Ive been clicked");
     //get current players total
 });
 
 $("#crystal-2").on("click", function() {
-    crystalClicked();
+    var randNum = parseInt(document.getElementById("crystal-2").alt);
+    crystalClicked(randNum);
     //alert("Ive been clicked");
     //get current players total
 });
 
 $("#crystal-3").on("click", function() {
-    crystalClicked();
+    var randNum = parseInt(document.getElementById("crystal-3").alt);
+    crystalClicked(randNum);
     //alert("Ive been clicked");
     //get current players total
 });
 
 $("#crystal-4").on("click", function() {
-    crystalClicked();
+    var randNum = parseInt(document.getElementById("crystal-4").alt);
+    crystalClicked(randNum);
     //alert("Ive been clicked");
     //get current players total
 });
 
 
 
-function crystalClicked() {
+function crystalClicked(randNum) {
     var newUserTotal = 0;
+    var tempWinHolder = 0;
     var tempLossHolder = 0;
+    var currentWinCount = 0;
     var currentLossCount = 0;
     var tempHolder = (document.getElementById("total-score").innerHTML);
     //console.log(tempHolder)
-    newUserTotal = (Number(tempHolder)) + pickRandomNumForCrystal();
+    newUserTotal = (Number(tempHolder)) + randNum;
     //console.log(newUserTotal);
     totalscore.textContent = newUserTotal;
 
+    if (newUserTotal === randVal) {
+        currentWinCount = parseInt((document.getElementById("win-count").innerHTML));
+        tempWinHolder = (Number(currentWinCount)) + 1;
+        // wincount.textContent = "Wins:    " + tempWinHolder.toString();
+        wincount.textContent = tempWinHolder.toString();
+        totalscore.textContent = "0";
+        loadCrystals();
+        setRandomNumber();
+    } else
     if (newUserTotal > randVal) {
         //player just lost
-        currentLossCount = (document.getElementById("loss-count").innerHTML);
+        currentLossCount = parseInt((document.getElementById("loss-count").innerHTML));
         //console.log("currentLossCount = " + currentLossCount);
         tempLossHolder = (Number(currentLossCount)) + 1;
         //console.log(tempLossHolder);
+        //losscount.textContent = "Losses: " + tempLossHolder.toString();
         losscount.textContent = tempLossHolder.toString();
-        totalscore.textContent = 0;
+        totalscore.textContent = "0";
+        loadCrystals();
         setRandomNumber();
     }
 
